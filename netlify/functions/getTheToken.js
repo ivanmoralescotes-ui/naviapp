92b637341ee7f83cabf3fcb4867c90561fa9dd46
ivan <ivanmoralescotes@gmail.com>
2025-12-04 16:34:32 -0500

    uploading file

---
+++ netlify/functions/getTheToken.js
@@ -0,0 +1,12 @@
+// archivo: functions/getGitHubToken.js
+
+exports.handler = async (event, context) => {
+  // Aquí obtienes el token desde la variable de entorno
+  const githubToken = process.env.THE_TOKEN;
+  
+  // Retornas el token (o idealmente, usas el token aquí para hacer alguna petición a GitHub y devolver el resultado)
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ token: githubToken })
+  };
+};
