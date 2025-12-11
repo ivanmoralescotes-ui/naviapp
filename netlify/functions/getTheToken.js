af94f46af531f6a062b8e6d697f3c1bc57573ccf
ivan <ivanmoralescotes@gmail.com>
2025-12-11 14:39:33 -0500

    cors header for function

--- netlify/functions/getTheToken.js
+++ netlify/functions/getTheToken.js
@@ -7,6 +7,10 @@ exports.handler = async (event, context) => {
   // Retornas el token (o idealmente, usas el token aquí para hacer alguna petición a GitHub y devolver el resultado)
   return {
     statusCode: 200,
+	headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization"
+    },
     body: JSON.stringify({ token: githubToken })
   };
 };
